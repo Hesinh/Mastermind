ebfc75f554952694422d31afdece801fac9a937a
Agus Ibacache <chocobomj@gmail.com>
2022-06-21 17:33:10 -0300

    still prototype, handling some ideas

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,13 +1,18 @@
+patter = [10, 2, 5, 4]
 class Mastermind
     @@chooses =[1, 2, 3, 4, 5, 6]
     @@pattern = [5, 2, 1, 4]
-
+    
+    def initialize(form)
+      @form = form
+    end
     def test
-      try = @@pattern.each {|element| element == @@chooses}
-      print try.length
+      try = (@@pattern & @form)
+      dab = try 
+      print dab
     end
 
 end
 
-len = Mastermind.new()
+len = Mastermind.new(patter)
 len.test
