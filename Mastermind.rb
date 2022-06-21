7a1e9077260111b4a9c0d7b25f52681a5cc92b27
Agus Ibacache <chocobomj@gmail.com>
2022-06-21 17:51:44 -0300

    still prototype : change of the names methods, fix position of items

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,18 +1,24 @@
-patter = [10, 2, 5, 4]
-class Mastermind
-    @@chooses =[1, 2, 3, 4, 5, 6]
-    @@pattern = [5, 2, 1, 4]
-    
-    def initialize(form)
-      @form = form
-    end
-    def test
-      try = (@@pattern & @form)
-      dab = try 
-      print dab
-    end
+guess = [10, 2, 5, 4]
 
+class Mastermind
+  @@chooses =[1, 2, 3, 4, 5, 6]
+  @@pattern = [5, 2, 1, 4]
+    
+  def initialize(guess)
+    @guess = guess
+  end
+  
+  def match
+    @try = (@@pattern & @guess)
+    print @try
+    square
+  end
+  
+  def square
+    grid = Array.new(@try.length, "white")
+    print grid
+  end
 end
 
-len = Mastermind.new(patter)
-len.test
+len = Mastermind.new(guess)
+len.match
