35404c5eac78be004a1629448661eb6e8b501dde
Agus Ibacache <chocobomj@gmail.com>
2022-06-22 16:00:35 -0300

    technically finished the project

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,22 +1,24 @@
 class Mastermind
-  a = (0..5).to_a.shuffle
-  a.pop
-  a.shift
-  @@pattern = a
+  
   private
   def initialize
-    puts "Welcome to MasterMind, you need to guess what is the pattern that i have, if your guess is correct in less than 9 TURNS , YOU WIN"
-    @guess
-    p @@pattern
+    cpu_guess
     start
   end
 
+  def cpu_guess
+    a = (0..5).to_a.shuffle
+    a.pop
+    a.shift
+    @pattern = a
+  end
+  
   def match
-    @size = (@@pattern & @guess)
+    @size = (@pattern & @guess)
     i = 0
     test = 0
     while i < 4
-      if @@pattern[i] == @guess[i]
+      if @pattern[i] == @guess[i]
         test += 1
       end
       i += 1
@@ -27,11 +29,16 @@ class Mastermind
 
   
   def start
-    i = 1
+    p @pattern
+    puts "Welcome to MasterMind, you need to guess what is the pattern that i have, if your guess is correct in 8 TURNS , YOU WIN"
     
+    i = 1
     while i < 9
       puts "put a number for the guess four times :TURN#{i}"
-      @guess = [gets.chomp.to_i,gets.chomp.to_i,gets.chomp.to_i,gets.chomp.to_i]
+      @guess = [gets.chomp.to_i,
+        gets.chomp.to_i,
+        gets.chomp.to_i,
+        gets.chomp.to_i]
       puts "your guess is #{@guess}"
       match
       i +=1
