171d795745761dd71088c1a4fd72ab81ef6ecafe
Agus Ibacache <chocobomj@gmail.com>
2022-06-21 23:05:44 -0300

    change some names and some methods

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,25 +1,37 @@
-guess = [10, 2, 5, 4]
-
 class Mastermind
   @@chooses = [0,1, 2, 3, 4, 5]
-  @@pattern = [rand(6), rand(6), rand(6), rand(6)]
+  @@pattern = [rand(6).to_s, rand(6).to_s, rand(6).to_s, rand(6).to_s]
     
-  def initialize(guess)
-    @guess = guess
+  def initialize
+    @guess
+    p @@pattern
+    start
   end
   
   def match
     @try = (@@pattern & @guess)
     print @try
     square
-    print @@pattern
+
   end
   
   def square
     grid = Array.new(@try.length, "white")
     print grid
   end
+  
+  def start
+    i = 0
+    
+    while i < 7
+      @guess = [gets.chomp,gets.chomp,gets.chomp,gets.chomp]
+      p @guess
+      match
+      puts "please, make an array with 4 numbers,separated with comma"
+    end
+  end
+  
 end
 
-len = Mastermind.new(guess)
+len = Mastermind.new
 len.match
